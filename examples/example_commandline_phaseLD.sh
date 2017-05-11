2d54ffd66a03872701c7bc14a5e0e8d7f49e26f7
MisterPotatoHead <marand@wisc.edu>
2017-05-11 11:34:24 -0400

    Updated examples

---
+++ examples/example_commandline_phaseLD.sh
@@ -0,0 +1,10 @@
+phaseLD.pl --in chr06.gen --out chr06_output --threads 50 --rpen 0.25 --win 2500 --quick_mode --bwin 250 --bstep 50
+
+## Note, you will need to adjust the value of threads according to the number of available CPUs
+## will output three files if phaseLD is installed properly
+## 
+## 1) chr06_output.log
+## 2) chr06_output.hap_raw
+## 3) chr06_output.bayes
+##
+## 
