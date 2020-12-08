e834620c2f77b371ece2976433bc5101cbdccd86
plantformatics <marand@uga.edu>
2020-12-08 13:59:49 -0500

    updated extract_crossovers.pl

--- bin/extract_crossovers.pl
+++ bin/extract_crossovers.pl
@@ -14,12 +14,12 @@ while(<F>){
 	chomp;
 	$rank++;
 	my @col = split("\t",$_);
-	for (my $i = 2; $i < @col; $i++){
-		my $id = join("-",$rank,$col[1]);
-		my $adj = $i - 1;
-		my @feat = split(":",$col[$i]);
-		my @best = split("=",$feat[0]);
-		$hash{$id}{$adj} = $best[0];
+	for (my $i = 1; $i < @col; $i++){     # caicc commented
+		my $id = join("-",$rank,$col[0]);   # caicc commented
+		my $adj = $i - 1;                   # caicc commented
+		my @feat = split(":",$col[$i]);     # caicc commented
+		my @best = split("=",$feat[0]);     # caicc commented
+		$hash{$id}{$adj} = $best[0];        # caicc commented
 	}
 }
 close F;
@@ -32,7 +32,7 @@ while(<G>){
 	my @col = split("\t",$_);
 	my $id = join("_",@col[0..1]);
 	for (my $i = 5; $i < @col; $i++){
-		my $adj = $i - 4;
+		my $adj = $i - 5;
 		$haps{$id}{$adj} = $col[$i];
 	}
 }
@@ -46,6 +46,11 @@ my @snps = nsort keys %haps;
 print STDERR "Begin crossover detection...\n";
 for (my $i = 0; $i < @wins - 1; $i++){
 	my $j = $i + 1; 
+	
+	# verbose
+	print STDERR " - running logistic regression for bins $wins[$i] - $wins[$j]\n";
+
+	# specify windows
 	my @win1a = split("-",$wins[$i]);
 	my @win2a = split("-",$wins[$j]);
 	my @win1 = split(":",$win1a[1]);
@@ -67,22 +72,15 @@ for (my $i = 0; $i < @wins - 1; $i++){
 	elsif($distance > 2000000 && ($start2 > $end1)){
 		next;
 	}
-#	if($start2 >= $start1 && $start2 <= $end1){
 	elsif($start2 < $end1 || $distance < 2000000){
 		my @inds = nsort keys %{$hash{$wins[$i]}};
 		for (my $n = 0; $n < @inds; $n++){
-			my $call1 = $hash{$wins[$i]}{$inds[$n]};
-			my $call2 = $hash{$wins[$j]}{$inds[$n]};
-			my @att1 = split(":",$call1);
-			my @att2 = split(":",$call2);
-			my @best1 = split("=",$att1[0]);
-			my @best2 = split("=",$att2[0]);
-			my $hap1 = $best1[0];
-			my $hap2 = $best2[0];
+			my $hap1 = $hash{$wins[$i]}{$inds[$n]}; # $call1
+			my $hap2 = $hash{$wins[$j]}{$inds[$n]}; # $call2
 			my $break = 0;
 			my %temp;
+			my %types;
 			if($hap1 ne $hap2){
-				print STDERR "$inds[$n]\t$hap1 $hap2\t$start1\t$end2\t";
 				for (my $t = 0; $t < @snps; $t++){
 					my @ids = split("_",$snps[$t]);
 					if($ids[1] >= $start1 && $ids[1] <= $end2){
@@ -91,12 +89,16 @@ for (my $i = 0; $i < @wins - 1; $i++){
 						}
 						else{
 							$temp{$snps[$t]} = $haps{$snps[$t]}{$inds[$n]};
-							$break++;
+							$types{$temp{$snps[$t]}} = 1;
 						}
 					}
 					elsif($ids[1] > $end2){
 						last;
 					}
+				}
+				my @uniqtypes = keys %types;
+				if(@uniqtypes > 1){
+					$break++;
 				}
 			}
 			my $little_break;
@@ -106,6 +108,8 @@ for (my $i = 0; $i < @wins - 1; $i++){
 				my @dummy;
 				my $cos = 0;
 				my @putative;
+				my @perf1;
+				my @perf2;
 				for (my $x = 0; $x < @sites; $x++){
 					my @coord = split("_",$sites[$x]);
 					my $adj = $x + 1;
@@ -117,18 +121,19 @@ for (my $i = 0; $i < @wins - 1; $i++){
 						my $call2 = $temp{$sites[$nxt]};
 						if($call1 ne $call2){
 							$cos++;
-							push(@putative, $sites[$x],$sites[$nxt]);
+							push(@putative, $sites[$x], $sites[$nxt]);
+							push(@perf1, $call1);
+							push(@perf2, $call2);
 						}	
 					}
 				}
 				if($cos == 1){
-					print STDERR "\n";
 					my @sites1 = split("_", $putative[0]);
 					my @sites2 = split("_",$putative[1]);
 					my $len = $sites2[1] - $sites1[1];
-					print "$sites1[0]\t$sites1[1]\t$sites2[1]\t1\t$inds[$n]\t$len\n";
+					print "$sites1[0]\t$sites1[1]\t$sites2[1]\t$perf1[0]=>$perf2[0]\t1\t$inds[$n]\t$len\n";
 				}
-				else{
+				elsif($cos > 1){
 					my $x = pdl @dummy;
 					my $y = pdl @calls;
 					my %results = $y->logistic($x);
@@ -140,30 +145,50 @@ for (my $i = 0; $i < @wins - 1; $i++){
 					my @probs = split/\s+/,$prob;
 					my $p_num = @probs;
 					my $m_num = @dummy;
-					print STDERR "Count markers = $m_num\tCount probs = $p_num\n";
-					my %diff;
+					my %test_diffs;
+					my %test_lengths;
 					for (my $t = 0; $t < @sites - 1; $t++){
-						my $j = $t + 1;
-						my $h1 = $temp{$sites[$t]};
-						my $h2 = $temp{$sites[$j]};
-						if($h1 ne $hap1 || $h2 ne $hap2){
-							next;
+						for (my $j = $t + 1; $j < @sites; $j++){
+							my $h1 = $temp{$sites[$t]};
+							my $h2 = $temp{$sites[$j]};
+							if($h1 ne $hap1 || $h2 ne $hap2){
+								next;
+							}
+							elsif(!$probs[$t] || !$probs[$j]){
+								next;
+							}
+							my $dif = abs($probs[$t] - $probs[$j]);
+							my $track = join("_",$t,$j);
+							my @ss1 = split("_",$sites[$t]);
+							my @ss2 = split("_",$sites[$j]);
+							my $len = abs($ss2[1]-$ss1[1]);
+							$test_lengths{$track} = $len;
+							$test_diffs{$track} = $dif;
 						}
-						elsif(!$probs[$t] || !$probs[$j]){
-							next;
-						}
-						my $dif = abs($probs[$t] - $probs[$j]);
-						my $track = join("_",$t,$j);
-						$diff{$track} = $dif;
 					}
-					my @breaks = sort {$diff{$b} <=> $diff{$a}} keys %diff;
-					my @ids = split("_",$breaks[0]);
+					my @keepers;
+					my $finalInt;
+					my @intervals = sort {$test_lengths{$a} <=> $test_lengths{$b}} keys %test_lengths;
+					foreach(@intervals){
+						if($test_diffs{$_} > 0.9){
+							push(@keepers, $_);
+						}
+					}
+					my @ids;
+					if(@keepers > 0){
+						@ids = split("_", $keepers[0]);
+						$finalInt = $keepers[0];
+					}else{
+						my @breaks = sort {$test_diffs{$b} <=> $test_diffs{$a}} keys %test_diffs;
+						@ids = split("_",$breaks[0]);
+						$finalInt = $breaks[0];
+					}
 					my $one = $ids[0];
 					my $two = $ids[1];
 					my @first = split("_", $sites[$one]);
 					my @secon = split("_", $sites[$two]);
 					my $length = $secon[1] - $first[1];
-					print "$first[0]\t$first[1]\t$secon[1]\t$hap1=>$hap2\t$diff{$breaks[0]}\t$inds[$n]\t$length\n";
+					print "$first[0]\t$first[1]\t$secon[1]\t$hap1=>$hap2\t$test_diffs{$finalInt}\t$inds[$n]\t$length\n";
 					for (my $z = 0; $z < @calls; $z++){
 						print $log "$inds[$n]\t$sites[$z]\t$calls[$z]\t$probs[$z]\n";
 					}
